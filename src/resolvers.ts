62ae56ee78984437a07fe5a22c386f7c77ec4be4
Gimel Dick <gimeldick@gmail.com>
2022-07-02 17:23:22 -0400

    fix invalid return type for context

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -37,7 +37,7 @@ export type ResolverConfig<T, S, _C, R, CR> = {
   input?: T extends object ? Record<keyof T, unknown> : unknown;
   schema?: Struct<T, S>;
   resolveContext?: ContextResolver<CR>;
-  resolve: (validatedInput: T extends null ? null : T, ctx: CR) => R;
+  resolve: (validatedInput: T extends null ? null : T, ctx: Awaited<CR>) => R;
 };
 
 /** Create a resolver */
